58da5661538af1aa90e58035fcc392ed4c158e7a
Catharsis-adm <vladygzargebin02@gmail.com>
2026-07-15 23:08:27 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from umaxbot import Bot, Dispatcher
 from umaxbot.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 # Берем токен из переменной окружения
-BOT_TOKEN = os.getenv("BOT_TOKEN", "f9LHodD0cOIDaws8b1l8j69CR1npvge7CYVSvTvk45VWeVyGtFoYYV3xggF7_XMHnpL_nr_rGfB5g0lMwqB_")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "f9Lh0dD0c0IDaws8b118j69CR1npvge7CYSvVtvk45VWeVgTfoYYV3xggF7_XMhInpL_nr_rGFB5g01MwuqB_")
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -14,6 +14,7 @@ async def on_message(message: Message):
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="📱 Поделиться номером", request_contact=True)]
     ])
+
     await bot.send_message(
         chat_id=message.sender.id,
         text="Привет! Нажми на кнопку, чтобы поделиться номером и получить подарок!",
